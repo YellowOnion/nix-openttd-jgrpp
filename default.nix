8eb7c57166601c44ad99d0590ee65eae9538b92a
Daniel Hill <daniel@gluo.nz>
2023-07-18 00:31:13 +1200

    make easy to build.

--- default.nix
+++ default.nix
@@ -1,15 +1,2 @@
-{ lib, fetchFromGitHub, openttd, zstd, ... }:
-
-openttd.overrideAttrs (oldAttrs: rec {
-  pname = "openttd-jgrpp";
-  version = "0.54.2";
-
-  src = fetchFromGitHub rec {
-    owner = "JGRennison";
-    repo = "OpenTTD-patches";
-    rev = "jgrpp-${version}";
-    hash =  "sha256-L6/n3DlywD7i7a2PcG9OVOH9+yAuplPUHrbCv+AyAcI=";
-  };
-
-  buildInputs = oldAttrs.buildInputs ++ [ zstd ];
-})
+{ pkgs ? import <nixpkgs> {}, ... }:
+(pkgs.callPackage ./package.nix {})
