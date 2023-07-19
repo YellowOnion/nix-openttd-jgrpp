ee0006ffc6c8ae112cc566cfc9d85b7604a9408e
Daniel Hill <daniel@gluo.nz>
2023-07-19 20:41:48 +1200

    update to 0.54.4

--- package.nix
+++ package.nix
@@ -2,14 +2,9 @@
 
 openttd.overrideAttrs (oldAttrs: rec {
   pname = "openttd-jgrpp";
-  version = "0.54.2";
+  version = "0.54.4";
 
-  src = fetchFromGitHub rec {
-    owner = "JGRennison";
-    repo = "OpenTTD-patches";
-    rev = "jgrpp-${version}";
-    hash =  "sha256-L6/n3DlywD7i7a2PcG9OVOH9+yAuplPUHrbCv+AyAcI=";
-  };
+  src = fetchFromGitHub (lib.importJSON ./version.json);
 
   buildInputs = oldAttrs.buildInputs ++ [ zstd ];
 })
