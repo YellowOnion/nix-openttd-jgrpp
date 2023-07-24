cc16cea7275eb1c908dc748eef6ab40203a394a5
Daniel Hill <daniel@gluo.nz>
2023-07-25 04:04:31 +1200

    launcher should be here.
    use yad to create a basic launcher for openttd versions
    
    add launcher to your `home.packages` or `environment.systemPackages`
    
    use the command `openttd-launcher` to run,
    
    for steam put `openttd-launcher %command%` in to steam launch commands

--- default.nix
+++ default.nix
@@ -1,5 +1,21 @@
 { pkgs ? import <nixpkgs> {}, ... }:
-{
+rec {
   jgr = pkgs.callPackage ./jgr.nix {};
   vanilla = pkgs.callPackage ./vanilla.nix {};
+  launcher = with pkgs;
+      (writeShellScriptBin "openttd-launcher"
+        ''
+        run_openttd () {
+            cd $1
+            ./bin/openttd
+        }
+        ${yad}/bin/yad --image ${vanilla}/share/icons/hicolor/128x128/apps/openttd.png \
+               --text "Which Version of OpenTTD?" \
+               --fixed \
+               --buttons-layout=center \
+               --button "Vanilla:0" \
+               --button "JGR patch-pack:1" \
+        && run_openttd ${vanilla} \
+        || run_openttd ${jgr}
+        '');
 }
