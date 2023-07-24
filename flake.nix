e685003e79e97ff732c303e0fc98bfdcffd34c31
Daniel Hill <daniel@gluo.nz>
2023-07-25 04:35:42 +1200

    migrate to flakes

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "bleeding edge openttd & launcher";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        openttd = import ./default.nix {inherit pkgs;};
+      in
+      {
+        packages = {
+          vanilla = openttd.vanilla;
+          jgr = openttd.jgr;
+          launcher = openttd.launcher;
+          default = openttd.launcher;
+        };
+      });
+}
