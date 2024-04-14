39990ac3791749707e2b4411b0131960da724219
Daniel Hill <daniel@gluo.nz>
2024-04-14 18:02:17 +1200

    bump vanilla 14.0

--- vanilla.nix
+++ vanilla.nix
@@ -2,9 +2,9 @@
 
 openttd.overrideAttrs (oldAttrs: rec {
   pname = "openttd";
-  version = "13.4";
+  version = "14.0";
   src = fetchurl {
     url = "https://cdn.openttd.org/openttd-releases/${version}/${pname}-${version}-source.tar.xz";
-    sha256 = "0vql1l25ym36vm4g76xl38kijf2gzb1m1x3ri0cf4n7y3fhfn79a";
+    sha256 = "1c8qhk44ba6x91xrrw8byywm1wzbws90dpla0cq5wsl1b2w6mxwn";
   };
 })
