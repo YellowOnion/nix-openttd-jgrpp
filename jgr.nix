59872be12db42c8cda922c110d65d91e3b0acb84
Daniel Hill <daniel@gluo.nz>
2024-09-06 15:38:54 +1200

    bump versions 2024-09-06

--- jgr.nix
+++ jgr.nix
@@ -2,7 +2,7 @@
 
 openttd.overrideAttrs (oldAttrs: rec {
   pname = "openttd-jgrpp";
-  version = "0.58.3";
+  version = "0.61.0";
 
   src = fetchFromGitHub (lib.importJSON ./jgr.json);
 
--- vanilla.nix
+++ vanilla.nix
@@ -2,9 +2,9 @@
 
 openttd.overrideAttrs (oldAttrs: rec {
   pname = "openttd";
-  version = "14.0";
+  version = "14.1";
   src = fetchurl {
     url = "https://cdn.openttd.org/openttd-releases/${version}/${pname}-${version}-source.tar.xz";
-    sha256 = "1c8qhk44ba6x91xrrw8byywm1wzbws90dpla0cq5wsl1b2w6mxwn";
+    sha256 = "151l05msgfknvfd2pa98n4p01nxw1ainkhc85i7qq5243zqch51c";
   };
 })
