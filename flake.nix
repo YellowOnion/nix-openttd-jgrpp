823bb4c48c71d54d255962cfc115cb0b8cd0fefc
Daniel Hill <daniel@gluo.nz>
2025-03-01 02:29:08 +1300

    pin my fixes branch till it gets merged

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "bleeding edge openttd & launcher";
   inputs = {
-    nixpkgs.url = "nixpkgs";
+    nixpkgs.url = "nixpkgs/d6c6be587af1dda37e82041f855368c8a507c0f6";
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = { self, nixpkgs, flake-utils }:
